8ec591fa9cbd62a94605e4c6a81b12480e93ee00
Philip Linden <lindenphilipj@gmail.com>
2024-07-21 13:31:05 -0400

    broken: making colors simpler

--- src/game/spawn/planet.rs
+++ src/game/spawn/planet.rs
@@ -1,6 +1,6 @@
 //! Spawn the player.
 
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 use crate::{
     game::{
@@ -9,6 +9,7 @@ use crate::{
         movement::{Movement, MovementController, WrapWithinWindow},
     },
     screen::Screen,
+    ui::palette,
 };
 
 pub(super) fn plugin(app: &mut App) {
@@ -26,22 +27,16 @@ pub struct Earth;
 fn spawn_player(
     _trigger: Trigger<SpawnEarth>,
     mut commands: Commands,
-    image_handles: Res<HandleMap<ImageKey>>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    // A texture atlas is a way to split one image with a grid into multiple sprites.
-    // By attaching it to a [`SpriteBundle`] and providing an index, we can specify which section of the image we want to see.
-    // We will use this to animate our player character. You can learn more about texture atlases in this example:
-    // https://github.com/bevyengine/bevy/blob/latest/examples/2d/texture_atlas.rs
-    let layout = TextureAtlasLayout::from_grid(UVec2::splat(32), 6, 2, Some(UVec2::splat(1)), None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-    let player_animation = PlayerAnimation::new();
 
     commands.spawn((
         Name::new("Earth"),
         Earth,
-        SpriteBundle {
-            texture: image_handles[&ImageKey::Ducky].clone_weak(),
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
+            material: materials.color(palette::GREEN),
             transform: Transform::from_scale(Vec2::splat(8.0).extend(1.0)),
             ..Default::default()
         },
--- src/ui/palette.rs
+++ src/ui/palette.rs
@@ -1,10 +1,16 @@
 use bevy::prelude::*;
 
-pub const BUTTON_HOVERED_BACKGROUND: Color = Color::srgb(0.2, 0.2, 0.2);
-pub const BUTTON_PRESSED_BACKGROUND: Color = Color::srgb(0.9, 0.9, 0.9);
+pub const GREEN: Color = Color::srgb(0.0, 1.0, 0.0);
+pub const YELLOW: Color =  Color::srgb(0.867, 0.827, 0.412);
+pub const WHITE: Color = Color::srgb(0.9, 0.9, 0.9);
+pub const LIGHT_GRAY: Color =Color::srgb(0.3, 0.3, 0.3);
+pub const DARK_GRAY: Color = Color::srgb(0.1, 0.1, 0.1);
+pub const BLACK: Color = Color::srgb(0.0, 0.0, 0.0);
 
-pub const BUTTON_TEXT: Color = Color::srgb(0.0, 1.0, 0.0);
-pub const LABEL_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
-pub const HEADER_TEXT: Color = Color::srgb(0.0, 1.0, 0.0);
+pub const NODE_BACKGROUND: Color = DARK_GRAY;
+pub const BUTTON_HOVERED_BACKGROUND: Color = LIGHT_GRAY;
+pub const BUTTON_PRESSED_BACKGROUND: Color = WHITE;
 
-pub const NODE_BACKGROUND: Color = Color::srgb(0.1, 0.1, 0.1);
+pub const BUTTON_TEXT: Color = GREEN;
+pub const LABEL_TEXT: Color = YELLOW;
+pub const HEADER_TEXT: Color = GREEN;
