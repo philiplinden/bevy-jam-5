8526ef6cb478e585ca5dc2a2b5e7d3e04f14c872
Philip Linden <lindenphilipj@gmail.com>
2024-07-26 06:18:20 -0400

    ui: might be broken

--- src/game/oscilloscope/waveform.rs
+++ src/game/oscilloscope/waveform.rs
@@ -1,13 +1,13 @@
 use bevy::prelude::*;
 
-use super::display::OscilloscopeMaterial;
+use super::{display::OscilloscopeMaterial, WaveformControls, XAxis};
 
 pub(super) fn plugin(app: &mut App) {
     app.register_type::<Waveform>();
     app.add_systems(Update, update_wave_form);
 }
 
-#[derive(Component, Reflect)]
+#[derive(Component, Reflect, Clone, Copy, Debug)]
 pub struct Waveform {
     pub amp: f32,
     pub freq: f32,
@@ -35,17 +35,17 @@ impl Waveform {
 
 
 
-pub fn update_wave_form(mut materials: ResMut<Assets<OscilloscopeMaterial>>, time: Res<Time>) {
+pub fn update_wave_form(
+    mut x_ctrl: Query<(&mut XAxis, &WaveformControls)>,
+    mut y_ctrl: Query<(&mut XAxis, &WaveformControls)>,
+    mut materials: ResMut<Assets<OscilloscopeMaterial>>,
+) {
+    let (x_axis, x_input) = x_ctrl.single_mut();
+    let (y_axis, y_input) = y_ctrl.single_mut();
+
     for (_id, material) in materials.iter_mut() {
-        let x = Waveform {
-            amp: 0.25,
-            phase: time.elapsed_seconds(),
-            ..default()
-        };
-        let y = Waveform {
-            freq: 1.5,
-            ..x
-        };
+        let x = x_axis.0;
+        let y = y_axis.0;
         let data: Vec<Vec2> = x.iter(0.0, 0.1)
                                .zip(y.iter(0.0, 0.1))
                                .take(100).map(|(x, y)| Vec2::new(x, y)).collect();
