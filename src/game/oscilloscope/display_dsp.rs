8f50e22f4f1bf51ad7d69bea45e25647eec1420a
Shane Celis <shane.celis@gmail.com>
2024-07-27 05:13:36 -0400

    style: Adjust octave.

--- src/game/oscilloscope/display_dsp.rs
+++ src/game/oscilloscope/display_dsp.rs
@@ -1,5 +1,5 @@
 use {bevy::prelude::*, bevy_fundsp::prelude::*, uuid::Uuid};
-use crate::game::audio::piano::{PianoId, PitchVar};
+use crate::game::audio::piano::{PianoId, PianoUnit, PitchVar};
 use crate::game::oscilloscope::OscilloscopeMaterial;
 
 pub fn plugin(app: &mut App) {
@@ -7,31 +7,43 @@ pub fn plugin(app: &mut App) {
 }
 
 /// Update the oscilloscope material to match the waveform's shape.
+// pub fn update_oscilloscope(
+//     dsp_manager: Res<DspManager>,
+//     // piano_ids: Query<&PianoId, Changed<PitchVar>>,
+//     piano_ids: Query<&PianoId>,
+//     mut materials: ResMut<Assets<OscilloscopeMaterial>>,
+// ) {
+
+//     for piano_id in &piano_ids {
+//     for (_id, material) in materials.iter_mut() {
+//         let mut audio_unit = dsp_manager
+//             .get_graph_by_id(&piano_id.0)
+//             .expect("graph");
+
+//         material.channels.clear();
+//         material.channels.extend(audio_unit.into_iter().take(1000).map(|[a, b]| Vec2::new(a,b)));
+//     }
+//     }
+// }
 pub fn update_oscilloscope(
     dsp_manager: Res<DspManager>,
-    // piano_ids: Query<&PianoId, Changed<PitchVar>>,
-    piano_ids: Query<&PianoId>,
-    // mut x_ctrl: Query<(&mut XAxis, &WaveformControls)>,
-    // mut y_ctrl: Query<(&mut XAxis, &WaveformControls)>,
+    mut piano_units: Query<&mut PianoUnit, Changed<PitchVar>>,
     mut materials: ResMut<Assets<OscilloscopeMaterial>>,
 ) {
 
-    // let (x_axis, x_input) = x_ctrl.single_mut();
-    // let (y_axis, y_input) = y_ctrl.single_mut();
-    for piano_id in &piano_ids {
-    for (_id, material) in materials.iter_mut() {
-        // let x = Waveform::default();
-        // let y = Waveform::default();
-        // let data: Vec<Vec2> = x.iter(0.0, 0.1)
-        //                        .zip(y.iter(0.0, 0.1))
-        //                        .take(100).map(|(x, y)| Vec2::new(x, y)).collect();
-        // material.channels = data;
-        let mut audio_unit = dsp_manager
-            .get_graph_by_id(&piano_id.0)
-            .expect("graph");
+    for mut piano_unit in &mut piano_units {
+        for (_id, material) in materials.iter_mut() {
 
-        material.channels.clear();
-        material.channels.extend(audio_unit.into_iter().take(1000).map(|[a, b]| Vec2::new(a,b)));
-    }
+            let input : [f32; 0] = [];
+            let mut output : [f32; 2] = [0., 0.];
+
+            piano_unit.set_sample_rate(4410.0); // instead of 44,100 Hz
+
+            material.channels.clear();
+            for i in 0..1000 {
+                piano_unit.tick(&input, &mut output);
+                material.channels.push(Vec2::new(output[0], output[1]));
+            }
+        }
     }
 }
